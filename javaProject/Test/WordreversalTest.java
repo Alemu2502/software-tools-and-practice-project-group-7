1180305ebd778e92ac9bbf3d8d7dfa87da11f06b
Alex <117904969+Alemu2502@users.noreply.github.com>
2024-04-04 10:21:19 +0300

    Modified By Alemu

--- javaProject/Test/WordreversalTest.java
+++ javaProject/Test/WordreversalTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class WordReversalTest {
