0f42aefe20e6e30ba52100eb0acf10b645e89b68
Biruk Girma <birukgmk@gmail.com>
2024-04-04 09:01:01 -0400

    group project

---
+++ javaProject/Test/PalindromeInWord.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeInWord {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = scanner.nextLine();
+        
+        if(isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+        
+        while(left < right) {
+            if(word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
